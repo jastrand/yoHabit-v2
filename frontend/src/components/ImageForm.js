8baea8e59fced054df9c6757db1bbea7173ae22b
Johanna <johannakastrand@gmail.com>
2020-06-21 20:02:30 +0200

    restored imageform

--- frontend/src/components/ImageForm.js
+++ frontend/src/components/ImageForm.js
@@ -1,39 +1,38 @@
-import React, { useState, useRef } from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { useParams } from 'react-router-dom'
 import styled from 'styled-components'
 import { userProfile } from '../reducers/userinfo'
 import { Input, Label, } from '../components/FormStyle'
 
 
-export const ImageForm = () => {
+export const ImageForm = (props) => {
   const id = useSelector((state) => state.userProfile.user.id)
   const dispatch = useDispatch()
-  const fileInput = useRef()
-
+  const [imageUrl, setImageUrl] = useState()
   const handleSubmit = (e) => {
     e.preventDefault()
-    const formData = new FormData()
-    formData.append('image', fileInput.current.files[0])
-
-    fetch(`http://localhost:8080/users/${id}/image`,
-      { method: 'POST', body: formData })
-      .then((res) => res.json())
+    props.function(false)
+    fetch(`http://localhost:8080/users/${id}`, {
+      method: 'POST',
+      body: JSON.stringify({ image: imageUrl }),
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then(res => res.json())
       .then((data) => {
-        dispatch(userProfile.actions.setProfile({ imageUrl: data.imageUrl }))
+        dispatch(userProfile.actions.setProfile({ profileImage: data.imageURL }))
       })
-
   }
-
   return (
-    <form onSubmit={(e) => handleSubmit(e)}>
-      <label>Update profile image
-      <input type="file" ref={fileInput} /></label>
-      <button
+    <Form onSubmit={(e) => handleSubmit(e)}>
+      <Label>Update profile image
+            <Input type='url'
+          onChange={(e) => setImageUrl(e.target.value)}
+        ></Input></Label>
+      <Button
         type='submit'>
         Add url
-            </button>
-    </form>
+            </Button>
+    </Form>
   )
 }
 
@@ -49,5 +48,17 @@ const Form = styled.form`
 `
 
 const Button = styled.button`
- 
+font-size: 20px;
+  background-color: transparent;
+  border-radius: 12px;
+  margin: 10px 0 10px 0;
+  color: #3831ac;
+  border-color: #3831ac;
+  padding: 14px;
+  &:hover {
+    background: #3831ac;
+    color: white;
+    transform: scale(1.1);
+    cursor: pointer;
+  }
 `
